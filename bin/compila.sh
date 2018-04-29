9777df39690b2c72e381ace7d0894f763297f52f
Andrea Ambrosini - Rossonet s.c.a r.l <andrea.ambrosini@rossonet.org>
2018-04-29 17:10:53 +0200

    debug compilazione

--- bin/compila.sh
+++ bin/compila.sh
@@ -6,8 +6,8 @@ export PATH_ARDUINO=$(pwd)/arduino-1.5.8
 
 echo "Uso Arduino 1.5.8 in $PATH_ARDUINO"
 
-ris334=$($PATH_ARDUINO/arduino --verify --board arduino:avr:due DW2040DUEF40SP1_REV1.3.3.4/DW2040DUEF40SP1_REV1.3.3.4.ino -v | grep DW2040DUEF40SP1_REV1.3.3.4.cpp.bin | cut -d\  -f 5)
-ris338=$($PATH_ARDUINO/arduino --verify  --board arduino:avr:due DW2040DUEF40SP1_REV1.3.3.8/DW2040DUEF40SP1_REV1.3.3.8.ino -v | grep DW2040DUEF40SP1_REV1.3.3.8.cpp.bin | cut -d\  -f 5)
+ris334=$($PATH_ARDUINO/arduino --verify --board arduino:sam:arduino_due_x DW2040DUEF40SP1_REV1.3.3.4/DW2040DUEF40SP1_REV1.3.3.4.ino -v | grep DW2040DUEF40SP1_REV1.3.3.4.cpp.bin | cut -d\  -f 5)
+ris338=$($PATH_ARDUINO/arduino --verify  --board arduino:sam:arduino_due_x DW2040DUEF40SP1_REV1.3.3.8/DW2040DUEF40SP1_REV1.3.3.8.ino -v | grep DW2040DUEF40SP1_REV1.3.3.8.cpp.bin | cut -d\  -f 5)
 
 rm -f DW2040DUEF40SP1_REV1.3.3.4.cpp.bin
 cp $ris334 .
